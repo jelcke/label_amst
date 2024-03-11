c14f298201c995d3607d31e7828ab9d669330652
jelcke <jelcke@xvector.io>
2024-03-11 04:38:07 +0100

    Add label generation and printing functionality

--- label.py
+++ label.py
@@ -1,5 +1,6 @@
 import json
 import os
+import shutil
 import cups
 import sys
 
@@ -21,63 +22,73 @@ def check_printer_status():
 
     print(f'Printer {printer_name} is online.')
 
+def generate_label(address):
+        zpl = f"""
+    ^XA
 
-def generate_label(data):
-    zpl = f"""
-^XA
+    ^FX Top section with logo, name and address.
+    ^CF0,60
+    ^FO50,50^GB100,100,100^FS
+    ^FO75,75^FR^GB100,100,100^FS
+    ^FO93,93^GB40,40,40^FS
+    ^FO220,50^FDAmstelbooks^FS
+    ^CF0,30
+    ^FO220,115^FDRoute du Moulin du Ranc 550^FS
+    ^FO220,155^FD07240 Verrnoux-en-Vivarais^FS
+    ^FO220,195^FDFrance^FS
+    ^FO50,250^GB700,3,3^FS
 
-^FX Top section with logo, name and address.
-^CF0,60
-^FO50,50^GB100,100,100^FS
-^FO75,75^FR^GB100,100,100^FS
-^FO93,93^GB40,40,40^FS
-^FO220,50^FDAmstelbooks^FS
-^CF0,30
-^FO220,115^FDRoute du Moulin du Ranc 550^FS
-^FO220,155^FD07240 Verrnoux-en-Vivarais^FS
-^FO220,195^FDFrance^FS
-^FO50,250^GB700,3,3^FS
+    ^FX Second section with recipient address and permit information.
+    ^CFA,30
 
-^FX Second section with recipient address and permit information.
-^CFA,30
-
-^CI28
-^FH
-^FO50,300^FDExpédier à / destination :^FS  
-^FO50,360^FD{data['name']}^FS
-^FO50,400^FD{data['street']}^FS
-^FO50,440^FD{data['city']} {data['postalCode']}^FS
-^FO50,480^FD{data['country']}^FS
-^FO50,540^FD{data['phone']}^FS
+    ^CI28
+    ^FH
+    ^FO50,300^FDExpédier à / destination :^FS  
+    ^FO50,360^FD{address['name']}^FS
+    ^FO50,400^FD{address['street']}^FS
+    ^FO50,440^FD{address['city']} {address['postal_code']}^FS
+    ^FO50,480^FD{address['country']}^FS
+    ^FO50,540^FD{address['phone']}^FS
 
 
-^CFA,15
+    ^CFA,15
 
-^FX Fourth section (the bottom box).
-^FO50,900^GB700,250,3^FS
-^CF0,40
+    ^FX Fourth section (the bottom box).
+    ^FO50,900^GB700,250,3^FS
+    ^CF0,40
 
-^FO100,960^FDHandle with care^FS
-^FO100,1000^FDContains books^FS
-^FO100,1040^FDContient des livres^FS
+    ^FO100,960^FDHandle with care^FS
+    ^FO100,1000^FDContains books^FS
+    ^FO100,1040^FDContient des livres^FS
 
-^XZ
-"""
-    return zpl
- 
+    ^XZ
+    """
+        return zpl
 
-def main():
-
-    check_printer_status()
-    with open('./json/addresses.json', 'r') as f:
+def process_json_file(json_file_path):
+    with open(json_file_path, 'r') as f:
         addresses = json.load(f)
 
-    for address in addresses:
-        label = generate_label(address)
-        with open('./labels/label.zpl', 'w') as f:
-            f.write(label)
-        os.system('/usr/bin/lprint submit -d ZebraGK420d ./labels/label.zpl')
-        # /usr/bin/lprint add -d ZebraGK420d -v usb://Zebra%20Technologies/ZTC%20GK420d?serial=28J133302347 -m zpl_4inch-300dpi-dt
+    for index, address in enumerate(addresses):
+        label_zpl = generate_label(address)
+        label_file_path = f'./labels/label_{os.path.basename(json_file_path).replace(".json", "")}_{index}.zpl'
+        with open(label_file_path, 'w') as f:
+            f.write(label_zpl)
+        os.system(f'/usr/bin/lprint submit -d ZebraGK420d {label_file_path}')
+
+def main():
+    check_printer_status()
+    json_folder = './json'
+    processed_folder = './json/processed'
+
+    if not os.path.exists(processed_folder):
+        os.makedirs(processed_folder)
+
+    for json_file in os.listdir(json_folder):
+        if json_file.endswith('.json'):
+            json_file_path = os.path.join(json_folder, json_file)
+            process_json_file(json_file_path)
+            shutil.move(json_file_path, os.path.join(processed_folder, json_file))
 
 if __name__ == "__main__":
     main()
